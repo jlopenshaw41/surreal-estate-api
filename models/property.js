ecd5918009d6ecd61d654e331a874c7292556114
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-03-10 14:26:23 +0000

    amended property model to include image and region

--- models/property.js
+++ models/property.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const PropertyListingSchema = new mongoose.Schema({
   title: { type: String, require: true },
+  image: { type: String, require: true},
   type: { type: String, require: true },
   bedrooms: { type: String, require: true },
   bathrooms: { type: String, require: true },
-  price: { type: String, require: true },
-  city: { type: String, require: true },
+  price: { type: Number, require: true },
+  region: { type: String, require: true },
   email: { type: String, require: true },
 });
 
